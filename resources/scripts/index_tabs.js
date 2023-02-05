307655be781f3bc591f1b326f6ea6ed7cb65bcae
Shivam <shivamjaiswal095@gmail.com>
2023-02-05 22:19:34 +0530

    v1.0

--- resources/scripts/index_tabs.js
+++ resources/scripts/index_tabs.js
@@ -1,17 +1,17 @@
-function openCity(evt, tabName) {
+function tabControl(evt, tabName) {
   // Declare all variables
-  var i, tabcontent, tablinks;
+  var i, tabcontent, tablink;
 
-  // Get all elements with class="tabcontent" and hide them
+  // Get all elements with class="tabContents" and hide them
   tabcontent = document.getElementsByClassName("tabContents");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
   // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  tablink = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablink.length; i++) {
+    tablink[i].className = tablink[i].className.replace(" active", "");
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
